4ea549c0471b199364bf822f83f17bafedf55398
Marcin Sawczuk-Szymkiewicz <sawczuk.marcin@gmail.com>
2019-05-07 22:26:54 +0200

    restilled and fuctionality added

--- cv.js
+++ cv.js
@@ -112,8 +112,50 @@ const navigate = e => {
     }
     setFocus();
 }
+
 document.onkeydown = navigate
 
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
+            if( focus > 0) focus--;
+        } else {
+            /* right swipe */
+            if(focus < sections.length - 1) focus++; 
+        }                       
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
 
 
 const colors = {
